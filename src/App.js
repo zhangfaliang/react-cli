3fa7c7bfd01d10ee51846afa54584cedfd82ecef
张发亮 <fl.zhang@8win.com>
2018-08-28 09:44:30 +0800

    add react-router history connected-react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import logo from './logo.svg';
+//import asyncComponent from './components/AsyncComponent';
+//聆听翻译 另外，我们在这里传递一个函数似乎很奇怪。
+//为什么不直接传入一个字符串（比如./ containers / Home）
+//然后在AsyncComponent中执行动态import（）？这是因为我们想要明确说明我们动态导入的组件。
+//Webpack基于此分割我们的应用程序。它查看这些导入并生成所需的部分（或块）。
+
+//const AsyncHome = asyncComponent(() => import("./containers/Home"));
+
 import './App.css';
 
 class App extends Component {
+  handleClick = e => {
+    import('./moduleA.js')
+      .then(({ moduleA }) => {
+        console.log(moduleA);
+      })
+      .catch(ree => {
+        console.log(ree);
+      });
+  };
+
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <title>Nested Title</title>
+          <meta name="description" content="Nested component" />
+        </Helmet>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro" onClick={this.handleClick}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createBrowserHistory } from 'history';
+import { Route, Switch } from 'react-router'; // react-router v4
+
+import {
+  connectRouter,
+  routerMiddleware,
+  ConnectedRouter
+} from 'connected-react-router';
+
 import './index.css';
 import App from './App';
+import rootReducer from './reducers/rootReducer';
 import registerServiceWorker from './registerServiceWorker';
-console.log(App);
+const history = createBrowserHistory();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  connectRouter(history)(rootReducer), //
+  {},
+  composeWithDevTools(applyMiddleware(routerMiddleware(history)))
+);
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" render={() => <App />} />
+        <Route exact path="/home" render={() => 'home'} />
+        <Route exact path="/app" render={() => 'app'} />
+
+        <Route render={() => <div>Miss</div>} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
